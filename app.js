5dbf26d57ac6122a5b231312bbe91ef01a40a16d
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-06-11 07:04:00 +0100

    added a few error handling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require("morgan");
 const userRouter = require('./Routes/userRouter')
-
+const AppError = require('./Errors/appError')
 const app = express();
 
 app.use(express.json());
@@ -15,5 +15,7 @@ app.use((req, res, next) => {
 
 app.use('/users', userRouter)
 
-
+app.all('*', (req, res, next) => {
+    next(new AppError(`can not find ${req.originalUrl} on this server`))
+})
 module.exports = app;
